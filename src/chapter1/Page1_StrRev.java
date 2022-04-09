bfb8cdf6432d8dff80a4587cdd324175ca0caacf
Jithendra <jithendra.narala@gmail.com>
2022-04-09 19:53:50 -0400

    Added String Reverse - 04092022

---
+++ src/chapter1/Page1_StrRev.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class Page1_StrRev {
+
+	public static void main(String[] args) {
+		String str = "Jithendra";
+		StringBuilder str1 = new StringBuilder();
+		str1.append(str);
+		System.out.println(str1.reverse());
+	}
+
+}
