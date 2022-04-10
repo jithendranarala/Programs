06db9017a8ae5f9f1b862e5c292fc07b54461573
Jithendra <jithendra.narala@gmail.com>
2022-04-10 06:50:31 -0400

    Char

---
+++ src/chapter1/AllChars.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class AllChars {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 200; i++) {
+			if (i >= 65 && i<= 90) {
+				System.out.println((char) i + " " + i);	
+			}
+
+		}
+
+	}
+
+}
