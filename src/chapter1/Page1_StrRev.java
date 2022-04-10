5fc4404d26c8321d1c6d835bc86cea6720a676f3
Jithendra <jithendra.narala@gmail.com>
2022-04-09 23:22:41 -0400

    Added StrRev3

--- src/chapter1/Page1_StrRev.java
+++ src/chapter1/Page1_StrRev.java
@@ -1,11 +1,14 @@
 package chapter1;
 
+import java.util.Scanner;
+
 public class Page1_StrRev {
 
 	public static void main(String[] args) {
 		Page1_StrRev pg1 = new Page1_StrRev();
 		pg1.strRev1();
 		pg1.strRev2();
+		pg1.strRev3();
 		}
 	
 	void strRev1() {
@@ -20,7 +23,19 @@ public class Page1_StrRev {
 		for (int i = str.length()-1 ; i >= 0 ; i--) {
 		 System.out.print(str.charAt(i));	
 		}
-		
 	}
+		void strRev3() {
+			System.out.println("");
+			Scanner sc = new Scanner(System.in);
+			String str1 = sc.nextLine();
+			String str2[] = str1.split("");
+			System.out.println(str2.length);
+			for (int i = str2.length; i > 0 ; i--) {
+				System.out.print(str2[i-1]);
+			}
+			
+		}
+		
+
 
 }
