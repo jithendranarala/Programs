d73141314b8e84f96614478693e4b31184edcfe2
Jithendra <jithendra.narala@gmail.com>
2022-04-10 06:59:44 -0400

    LowertoUppper

--- src/chapter1/LowerToUpper.java
+++ src/chapter1/LowerToUpper.java
@@ -4,16 +4,23 @@ public class LowerToUpper {
 
 	public static void main(String[] args) {
 		String str = "jithendra";
+		System.out.println(str.toUpperCase());
+		lowertoUpper(str);
+		
+		
+	}
+
+	static void lowertoUpper(String str) {
 		char str1[] = str.toCharArray();
- 		for (int i = 0; i < str1.length; i++) {
-			if (str1[i] >= 'a' && str1[i] <= 'z'){
-				str1[i]= (char)((int)str1[i] -32);
+		for (int i = 0; i < str1.length; i++) {
+			if (str1[i] >= 'a' && str1[i] <= 'z') {
+				str1[i] = (char) ((int) str1[i] - 32);
 			}
 		}
- 		System.out.println("Upper case");
- 		for (int i = 0; i < str1.length; i++) {
- 			System.out.print(str1[i]);
-			
+		System.out.println("Upper case");
+		for (int i = 0; i < str1.length; i++) {
+			System.out.print(str1[i]);
+
 		}
 	}
 
