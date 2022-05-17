4ae905d13ac4362e7f4936aa12db0f0c30b3a68f
jithendranarala <jithendra.narala@gmail.com>
2022-05-17 05:41:27 -0500

    Test1

--- src/Chapter4_BasicPrograms/IntergerPalindrome.java
+++ src/Chapter4_BasicPrograms/IntergerPalindrome.java
@@ -3,8 +3,7 @@ package Chapter4_BasicPrograms;
 public class IntergerPalindrome {
 
 	public static void main(String[] args) {
-		System.out.println("Test1");
-		System.out.println("abc");
+		System.out.println("Polidrame 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");
 	}
 
 }
