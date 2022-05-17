ef379d38544f4427856efa87dce0b1e9b911fedb
Jithendra <jithendra.narala@gmail.com>
2022-05-17 05:07:26 -0500

    Test Push

--- src/Chapter4_BasicPrograms/IntergerPalindrome.java
+++ src/Chapter4_BasicPrograms/IntergerPalindrome.java
@@ -3,8 +3,7 @@ package Chapter4_BasicPrograms;
 public class IntergerPalindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Test");
 	}
 
 }
